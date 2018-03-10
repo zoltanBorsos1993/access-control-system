ac14d78d619a07a0c83ea30666626b6706039b16
Zoltán Borsós <zoltan.borsos.1993@gmail.com>
2018-03-10 18:48:22 +0100

    Use docker multistage build for building modules (mod_auth)

---
+++ mod_auth/src/app/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!\n";
+  std::cout << "This is a demo!\n";
+  std::cout << "Now I am in a sub-directory, hiding from being built.\n";
+  std::cout << "Changed sources!\n";
+  std::cout << "Bye-bye!\n";
+}
