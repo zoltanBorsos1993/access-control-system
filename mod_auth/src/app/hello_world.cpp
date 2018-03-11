c3ed82a3bb9be4cf2d002633e7445230047244f9
Zoltán Borsós <zoltan.borsos.1993@gmail.com>
2018-03-11 13:41:16 +0100

    Building & uploading images with scripts

--- mod_auth/src/app/hello_world.cpp
+++ mod_auth/src/app/hello_world.cpp
@@ -4,7 +4,5 @@ int main()
 {
   std::cout << "Hello World!\n";
   std::cout << "This is a demo!\n";
-  std::cout << "Now I am in a sub-directory, hiding from being built.\n";
-  std::cout << "Changed sources!\n";
   std::cout << "Bye-bye!\n";
 }
