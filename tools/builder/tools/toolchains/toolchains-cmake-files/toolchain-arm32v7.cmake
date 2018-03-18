7d6b9a62928f6f9ae56245a45e6c7703df45047c
Zoltán Borsós <zoltan.borsos.1993@gmail.com>
2018-03-18 14:30:44 +0100

    Auth module builds correctly with ZMQ/CZMQ

---
+++ tools/builder/tools/toolchains/toolchains-cmake-files/toolchain-arm32v7.cmake
@@ -0,0 +1,14 @@
+# Define our host system
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+SET(TOOLCHAINS_PATH $ENV{TOOLCHAINS_PATH})
+
+# Define the cross compiler locations
+SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+
+# Use our definitions for compiler tools
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# Search for libraries and headers in the target directories only
+add_definitions(-Wall)
